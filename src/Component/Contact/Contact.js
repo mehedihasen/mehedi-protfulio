be2d23ec8430087b7355a0471bb24c733e3b77ca
mehedihasen <mehedisabuj4@gmail.com>
2021-05-09 05:14:47 +0800

    sdufhusi

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
         email: email.value,
         message: message.value,
       };
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("https://shrouded-beach-36584.herokuapp.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
